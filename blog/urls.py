5fa1c45f3f68cc301bcb68b933f9a8e6b05eaa75
nerdupunch <chineduopara@punch.cool>
2022-03-31 19:25:27 +0100

    initial commit

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.PostList.as_view(), name = "home"),
+    path('<slug:slug>/', views.DetailView.as_view(), name = 'detail'),
+
+]
